d5ded6211c173ec91f575f4fb4a15753ccee9c15
hector-wq-as <hectordejesusalonsorubio@gmail.com>
2025-09-18 13:32:46 -0600

    Add files via upload

---
+++ Simulacion_sensor_temperatura/JS/temp.js
@@ -0,0 +1,6 @@
+function cambiarTemperatura(){
+    let temperatura=document.getElementById("temp");
+    temperatura.innerText = (20+(Math.random()*10)).toFixed(2);
+    
+}
+setInterval(cambiarTemperatura, 2000);
